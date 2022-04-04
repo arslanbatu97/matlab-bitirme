ebb2a64ffdd6b06f47c010711ede49fe376e2df1
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-04 12:26:57 +0300

    Update detection.m

---
+++ detection.m
@@ -0,0 +1,11 @@
+v = VideoReader("people.mp4");
+
+i = 0;
+
+while hasFrame(v)
+    frame = readFrame(v);
+    gray_frame = rgb2gray(frame)
+    binary_frame = imbinarize(gray_frame);;
+    file = sprintf("a_frame_%d.png",i);
+    imwrite(binary_frame);
+    i = i+1
